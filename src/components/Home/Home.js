134d695b4f8c3dec28849eb294b213a64c7adc17
coderowson <runurowson@gmail.com>
2022-04-23 21:28:10 +0600

    show in react UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users, setusers] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setusers(data))
+    }, [])
     return (
         <div>
-
+            <h2>Available Users: {users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li key={user._id}>{user.name}:: {user.email}
+                        <button>X</button></li>)
+                }
+            </ul>
         </div>
     );
 };
