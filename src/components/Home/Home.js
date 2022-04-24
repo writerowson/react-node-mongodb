aee4024500c012dc987fc2fb7796dee681c1ff9b
coderowson <runurowson@gmail.com>
2022-04-24 10:52:34 +0600

    delete user

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,14 +7,37 @@ const Home = () => {
             .then(res => res.json())
             .then(data => setusers(data))
     }, [])
+
+    const hadleUserDelete = id => {
+        const proceed = window.confirm('Are you sure you want to delete it');
+        if (proceed) {
+            console.log('delete', id);
+            const url = `http://localhost:5000/user/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted');
+                        const reamaining = users.filter(user => user._id !== id)
+                        setusers(reamaining)
+                    }
+                })
+        }
+    }
+
+
     return (
         <div>
             <h2>Available Users: {users.length}</h2>
             <ul>
                 {
                     users.map(user => <li key={user._id}>{user.name}:: {user.email}
-                        <button>X</button></li>)
+                        {/* here we need parameter to specifically delete the element */}
+                        <button onClick={() => hadleUserDelete(user._id)}>X</button></li>)
                 }
+
             </ul>
         </div>
     );
